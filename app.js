e751c1fd0c92391f73bec81ab327baff1bc9d4ee
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-08-23 22:33:04 -0400

    inquirer actually works

--- app.js
+++ app.js
@@ -77,10 +77,7 @@ function addIntern() {
     },
   ]);
 }
-// function askQuestions() {
-//   inquirer.prompt(employeeQuestions);
-// }
-// askQuestions();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
